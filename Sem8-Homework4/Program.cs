105e16201b97d55d8d9baa68bbb10f8775c9a622
Ilya Grigorev <facekn110388@gmail.com>
2022-10-30 13:24:21 +0300

    Треугольник Паскаля

---
+++ Sem8-Homework4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника\
+
+float Factorial(int n)
+{
+    float i, result = 1;
+    for (i = 1; i < n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+
+Console.Write("Введите количество строк треугольника: ");
+int stroka = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+Console.WriteLine();
+
+
+
+for (int i = 0; i < stroka; i++)
+{
+    for (int c = 0; c <= (stroka - i); c++)
+    {
+        Console.Write(" ");
+    }
+    for (int c = 0; c <= i; c++)
+    {
+        Console.Write(" ");
+        Console.Write(Factorial(i) / (Factorial(c) * Factorial(i - c)));
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(); 
+
